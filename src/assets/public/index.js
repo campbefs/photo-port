b590f11f99507cc02b94879dbdf24dd0065faf44
Christopher Campbell <campbefs@gmail.com>
2021-06-21 22:28:59 -0700

    created modal

---
+++ src/assets/public/index.js
@@ -0,0 +1,49 @@
+import commercial_0 from '../small/commercial/0.jpg';
+import commercial_1 from '../small/commercial/1.jpg';
+import commercial_2 from '../small/commercial/2.jpg';
+import commercial_3 from '../small/commercial/3.jpg';
+import commercial_4 from '../small/commercial/4.jpg';
+
+import food_0 from '../small/food/0.jpg';
+import food_1 from '../small/food/1.jpg';
+import food_2 from '../small/food/2.jpg';
+import food_3 from '../small/food/3.jpg';
+import food_4 from '../small/food/4.jpg';
+
+import landscape_0 from '../small/landscape/0.jpg';
+import landscape_1 from '../small/landscape/1.jpg';
+import landscape_2 from '../small/landscape/2.jpg';
+import landscape_3 from '../small/landscape/3.jpg';
+import landscape_4 from '../small/landscape/4.jpg';
+
+import portraits_0 from '../small/portraits/0.jpg';
+import portraits_1 from '../small/portraits/1.jpg';
+import portraits_2 from '../small/portraits/2.jpg';
+import portraits_3 from '../small/portraits/3.jpg';
+
+const album = {
+  commercial_0,
+  commercial_1,
+  commercial_2,
+  commercial_3,
+  commercial_4,
+  
+  food_0,
+  food_1,
+  food_2,
+  food_3,
+  food_4,
+  
+  landscape_0,
+  landscape_1,
+  landscape_2,
+  landscape_3,
+  landscape_4,
+  
+  portraits_0,
+  portraits_1,
+  portraits_2,
+  portraits_3
+}
+
+export default album;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,53 +1,6 @@
 import React, { useState } from 'react';
 import Modal from '../Modal';
-
-import commercial_0 from '../../assets/small/commercial/0.jpg';
-import commercial_1 from '../../assets/small/commercial/1.jpg';
-import commercial_2 from '../../assets/small/commercial/2.jpg';
-import commercial_3 from '../../assets/small/commercial/3.jpg';
-import commercial_4 from '../../assets/small/commercial/4.jpg';
-
-import food_0 from '../../assets/small/food/0.jpg';
-import food_1 from '../../assets/small/food/1.jpg';
-import food_2 from '../../assets/small/food/2.jpg';
-import food_3 from '../../assets/small/food/3.jpg';
-import food_4 from '../../assets/small/food/4.jpg';
-
-import landscape_0 from '../../assets/small/landscape/0.jpg';
-import landscape_1 from '../../assets/small/landscape/1.jpg';
-import landscape_2 from '../../assets/small/landscape/2.jpg';
-import landscape_3 from '../../assets/small/landscape/3.jpg';
-import landscape_4 from '../../assets/small/landscape/4.jpg';
-
-import portraits_0 from '../../assets/small/portraits/0.jpg';
-import portraits_1 from '../../assets/small/portraits/1.jpg';
-import portraits_2 from '../../assets/small/portraits/2.jpg';
-import portraits_3 from '../../assets/small/portraits/3.jpg';
-
-const album = {
-  commercial_0,
-  commercial_1,
-  commercial_2,
-  commercial_3,
-  commercial_4,
-  
-  food_0,
-  food_1,
-  food_2,
-  food_3,
-  food_4,
-  
-  landscape_0,
-  landscape_1,
-  landscape_2,
-  landscape_3,
-  landscape_4,
-  
-  portraits_0,
-  portraits_1,
-  portraits_2,
-  portraits_3
-}
+import album from '../../assets/public';
 
 
 const PhotoList = ({ category }) => {
@@ -150,17 +103,27 @@ const PhotoList = ({ category }) => {
     },
   ]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const toggleModal = (image, i) => {
+    // current photo
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(true);
+  }
 
   return (
     <div>
-      <Modal />
+      {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={album[`${category}_${i}`]}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
